010290d3d50773e77de855060ee929cc03f54412
anojan1996 <anojan2012@gmail.com>
2021-05-01 22:13:15 +0530

    Anonymous Sign in

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,9 @@
-import 'package:brew_crew/screens/home/home.dart';
+import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
 
 class Wrapper  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Home();
+    return Authenticate();
   }
 }
