00a138394d712374a7466e901836c7dd3639d8f2
anojan1996 <anojan2012@gmail.com>
2021-05-03 21:12:39 +0530

    sign Out

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,9 +1,20 @@
+import 'package:brew_crew/models/user.dart';
 import 'package:brew_crew/screens/authenticate/authenticate.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:brew_crew/screens/home/home.dart';
 
 class Wrapper  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Authenticate();
+
+    final user = Provider.of<User>(context);
+    
+    //return either Home or Authenticate widget
+    if (user == null) {
+      return Authenticate();
+    } else {
+      return Home();
+    }
   }
 }
