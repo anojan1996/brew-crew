2cf3c0a522bb41758a145d0b8ce6f8e9c11ef6b7
anojan1996 <anojan2012@gmail.com>
2021-05-04 22:46:33 +0530

    Sign in with email and password

--- lib/screens/authenticate/sign_in.dart
+++ lib/screens/authenticate/sign_in.dart
@@ -10,10 +10,12 @@ class SignIn extends StatefulWidget {
 
 class _SignInState extends State<SignIn> {
   final AuthService _auth = AuthService();
+  final _formKey = GlobalKey<FormState>();
 
   // test field state
   String email = '';
   String password = '';
+  String error = '';
   
     @override
   Widget build(BuildContext context) {
@@ -36,10 +38,12 @@ class _SignInState extends State<SignIn> {
       body: Container(
         padding: EdgeInsets.symmetric(vertical: 20.0, horizontal: 50.0),
           child:Form(
+             key: _formKey,
             child: Column(
               children: <Widget>[
                 SizedBox(height: 20.0),
                 TextFormField(
+                  validator: (val) => val.isEmpty ? 'Enter an email' : null,
                   onChanged: (val) {
                     setState(() => email = val);
 
@@ -48,6 +52,7 @@ class _SignInState extends State<SignIn> {
                 SizedBox(height: 20.0),
                 TextFormField(
                   obscureText: true,
+                  validator: (val) => val.length<6 ? 'Enter a password 6+ chars long' : null,
                   onChanged: (val) {
                     setState(() => password = val);
 
@@ -61,10 +66,20 @@ class _SignInState extends State<SignIn> {
                     style:TextStyle(color: Colors.white),
                   ),
                   onPressed: () async{
-                    print(email);
-                    print(password);
+                    if (_formKey.currentState.validate()){
+                      dynamic result = await _auth.signInWithEmailAndPassword(email,password);
+                    
+                     if(result == null){
+                       setState(() => error = 'Could Not Sign In With Those Credentials');
+                     }
+                    }
 
                   },
+                  ),
+                  SizedBox(height: 12.0),
+                  Text(
+                    error,
+                    style: TextStyle(color: Colors.red, fontSize:14.0),
                   )
               ],),
           )
