fbda5158fb619158de99b5f48a235f40fe6b66ac
anojan1996 <anojan2012@gmail.com>
2021-05-05 10:01:07 +0530

    Text Input Decoration

--- lib/screens/authenticate/register.dart
+++ lib/screens/authenticate/register.dart
@@ -1,4 +1,5 @@
 import 'package:brew_crew/services/auth.dart';
+import 'package:brew_crew/shared/constants.dart';
 import 'package:flutter/material.dart';
 
 class Register extends StatefulWidget {
@@ -43,6 +44,7 @@ final _formKey = GlobalKey<FormState>();
               children: <Widget>[
                 SizedBox(height: 20.0),
                 TextFormField(
+                  decoration: textInputDecoration.copyWith(hintText: 'Email'),
                   validator: (val) => val.isEmpty ? 'Enter an email' : null,
                   onChanged: (val) {
                     setState(() => email = val);
@@ -51,6 +53,7 @@ final _formKey = GlobalKey<FormState>();
                 ),
                 SizedBox(height: 20.0),
                 TextFormField(
+                   decoration: textInputDecoration.copyWith(hintText: 'Password'), 
                   obscureText: true,
                    validator: (val) => val.length<6 ? 'Enter a password 6+ chars long' : null,
                   onChanged: (val) {
